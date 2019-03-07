f15a8f743df6e3174035447334935228988ba099
Cliff Weiler <cweiler52@gmail.com>
2019-03-07 11:30:31 -0500

    starting changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayProductsComponent } from './display-products/display-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayProductsComponent
   ],
   imports: [
     BrowserModule,
