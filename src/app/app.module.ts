257b8b2b2203b0585c5145095cead01b1a8d9811
Callie Riggs <callieriggs@Callies-MacBook-Air.local>
2019-03-08 09:12:28 -0500

    homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DesignHomeComponent } from './design-home/design-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DesignHomeComponent
   ],
   imports: [
     BrowserModule,
