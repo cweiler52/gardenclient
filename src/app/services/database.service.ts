524b7f624eb3fe6cf097948fd54d0a6a014dafc8
Cliff Weiler <cweiler52@gmail.com>
2019-03-07 22:56:38 -0500

    successfully added a working login form

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -23,21 +23,17 @@ export class DatabaseService {
     return this.http.get<Plant[]>(this.dbProductsUrl);
   }
 
-  login(email: string, password: string) {
-    return this.http.post<any>(this.dbAuthUrl, { email: email, password: password })
-        .pipe(map(user => {
-            // login successful if there's a jwt token in the response
-            if (user && user.token) {
-                // store user details and jwt token in local storage to keep user logged in between page refreshes
-                localStorage.setItem('token', user.token);
-            }
-
-            return user;
-        }));
+  loginUser(user) {
+    return this.http.post<any>(this.dbAuthUrl, user)
+      .pipe(map(user => {
+         if (user && user.token) {
+             localStorage.setItem('token', user.token);
+         }
+         return user;
+      }));
   }
 
   logout() {
-      // remove user from local storage to log user out
       localStorage.removeItem('token');
   }
 
