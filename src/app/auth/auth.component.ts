7523482a5a5c00906e1c79048fa3a03670b61c41
Arrhe <arrhenr@gmail.com>
2019-03-07 21:32:45 -0500

    evening push login incomplete

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import {DatabaseService} from '../services/database.service';
+import { first } from 'rxjs/operators';
+import { Router, ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -9,10 +11,33 @@ import {DatabaseService} from '../services/database.service';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  user = [];
+  login: FormGroup;
 
-  constructor() { }
+  constructor(
+      private fb: FormBuilder, 
+      private dbService: DatabaseService,
+      private route: ActivatedRoute,
+      private router: Router,
+      ) { }
 
   ngOnInit() {
+    this.login = this.fb.group({
+      email: new FormControl(),
+      password: new FormControl(),
+    })
+
   }
 
+  onLogin() : void {
+    this.dbService.login(this.email.value, this.password.value)
+    .pipe(first())
+    .subscribe(
+        data => {
+            this.router.navigate([this.returnUrl]);
+        },
+        error => {
+            this.alertService.error(error);
+        });
+  }
 }
--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Plant } from '../models/plant.model';
+import { User } from '../models/user.model';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -21,4 +23,22 @@ export class DatabaseService {
   getPlants() : Observable<Plant[]> {
     return this.http.get<Plant[]>(this.dbProductsUrl);
   }
-}
+
+  login(email: string, password: string) {
+    return this.http.post<any>(this.dbAuthUrl, { email: email, password: password })
+        .pipe(map(user => {
+            // login successful if there's a jwt token in the response
+            if (user && user.token) {
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('token', user.token);
+            }
+
+            return user;
+        }));
+  }
+
+  logout() {
+      // remove user from local storage to log user out
+      localStorage.removeItem('token');
+  }
+};
